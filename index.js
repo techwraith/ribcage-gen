d682dc5c9ae1ce2ae79f4c8192f8ef0ba9a97037
Daniel Erickson <techwraithpdx@gmail.com>
2014-05-02 21:27:57 -0700

    rename some things

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var hbsDir = require('hbs-dir')
+var change = require('change-case')
+var path = require('path')
 
 module.exports = function (target, cb) {
-  hbsDir('./compontent-template', target, cb)
+  var opts = {
+    origin: path.join(__dirname, './compontent-template/')
+  , target: path.join(process.cwd(), target)
+  , context: {
+      CamelName: change.pascalCase(target)
+    , name: target
+    }
+  }
+  hbsDir(opts, cb)
 }

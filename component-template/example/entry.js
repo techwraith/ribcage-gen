4e3f77d90cb18a618bcba4cb4d5a8a22ae5d2fa4
Joey Baker <joey@byjoeybaker.com>
2014-05-19 17:40:03 -0700

    Example now shows something by default

--- component-template/example/entry.js
+++ component-template/example/entry.js
@@ -1,7 +1,13 @@
 var $ = require('jquery')
   , Backbone = require('backbone')
   , {{PascalName}} = require('../index.js')
+  , $body = $('body')
+  , {{camelName}}
 
 Backbone.$ = $
 
+
 // Show us how the {{PascalName}} component works
+camelName = new PascalName({
+  el: $body
+})

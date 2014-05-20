4da014438ea88e0223b5ab6a380ba39cc136de3c
Joey Baker <joey@byjoeybaker.com>
2014-05-19 17:24:19 -0700

    Allow paths to be passed
    No more `cd components`!

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var change = require('change-case')
 var path = require('path')
 
 module.exports = function (target, cb) {
-  var opts = {
+  var nameParts = target.split(path.sep)
+  , name = nameParts[nameParts.length - 1]
+  , opts = {
     origin: path.join(__dirname, './component-template/')
   , target: path.join(process.cwd(), target)
   , context: {
-      PascalName: change.pascalCase(target)
-    , camelName: change.camelCase(target)
-    , name: target
+      PascalName: change.pascalCase(name)
+    , camelName: change.camelCase(name)
+    , name: name
     }
   }
   hbsDir(opts, cb)

687d142c67f9cd9f104ba1495e7d299a201ebc18
Joey Baker <joey@byjoeybaker.com>
2014-09-29 13:24:10 -0700

    Allow for different types of templates

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var hbsDir = require('hbs-dir')
-var change = require('change-case')
-var path = require('path')
+'use strict';
 
-module.exports = function (target, cb) {
-  var nameParts = target.split(path.sep)
+var hbsDir = require('hbs-dir')
+  , change = require('change-case')
+  , path = require('path')
+  , typeMap = {
+    backbone: 'backbone-component-template'
+    , react: 'react-component-template'
+  }
+
+// aliases for the type map
+typeMap.jsx = typeMap.react
+
+module.exports = function (options, cb) {
+  if (!options) throw new Error('optionsa are required')
+
+  var nameParts = options.target.split(path.sep)
   , name = nameParts[nameParts.length - 1]
   , opts = {
-    origin: path.join(__dirname, './component-template/')
-  , target: path.join(process.cwd(), target)
-  , context: {
-      PascalName: change.pascalCase(name)
-    , camelName: change.camelCase(name)
-    , name: name
+    origin: path.join(__dirname, typeMap[options.type])
+    , target: path.join(process.cwd(), options.target)
+    , context: {
+        PascalName: change.pascalCase(name)
+      , camelName: change.camelCase(name)
+      , name: name
+      }
     }
-  }
-  hbsDir(opts, cb)
+
+  hbsDir(opts, cb.bind(null, opts.context))
 }

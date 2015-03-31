aeb4b1a3b21deb1157d44ed71a3a500246fac0ec
Jonathan El-Bizri <github@elbizri.com>
2015-03-31 12:59:03 -0700

    whole lotta semicolons

--- backbone-component-template/example/entry.js
+++ backbone-component-template/example/entry.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var $ = require('jquery')
   , Backbone = require('backbone')
   , {{PascalName}} = require('../index.js')
--- backbone-pane-template/step.js
+++ backbone-pane-template/step.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 var Base = require('ribcage-view')
   , _ = require('lodash')

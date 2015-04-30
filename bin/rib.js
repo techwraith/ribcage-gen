7f0d19599ab3974ff3a305b8373234378c53dd41
Joey Baker <joey@byjoeybaker.com>
2015-04-30 10:37:29 -0700

    linter #cleanup

--- bin/rib.js
+++ bin/rib.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
 var gen = require('../index')
   , argv = require('minimist')(process.argv.slice(2))
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 var hbsDir = require('hbs-dir')
   , change = require('change-case')
@@ -12,7 +12,7 @@ var hbsDir = require('hbs-dir')
 // aliases for the type map
 typeMap.jsx = typeMap.react
 
-module.exports = function (options, cb) {
+module.exports = function ribcageGen (options, cb) {
   if (!options) throw new Error('options are required')
   if (!typeMap[options.type]) throw new Error('type ' + options.type + ' not found')
 
@@ -23,13 +23,13 @@ module.exports = function (options, cb) {
     , target: path.join(process.cwd(), options.target)
     , context: {
         PascalName: change.pascalCase(name)
-      , camelName: change.camelCase(name)
-      , titleName: change.titleCase(name)
-      , name: name
+        , camelName: change.camelCase(name)
+        , titleName: change.titleCase(name)
+        , name: name
       }
-    }
-  , successCallback = function successCallback (){
-      cb(opts.context)
+  }
+  , successCallback = function successCallback () {
+    cb(opts.context)
   }
 
   hbsDir(opts, successCallback)

c75d8b313e5b8999202511e9bdeacc9c469f226a
Joey Baker <joey@byjoeybaker.com>
2015-05-02 15:14:26 -0700

    more fixes for react index.jsx

--- react-component-template/index.jsx
+++ react-component-template/index.jsx
@@ -1,14 +1,14 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 const namespace = '{{camelName}}'
 
-export default class {{PascalName}} extends React.Component {
-  render(){
-    return <div className={namespace}>
+export default class {{PascalName}} extends Component {
+  render () {
+    return (<div className={namespace}>
       <h1 className={`${namespace}-title`}>{this.props.name} component</h1>
       <img src={`//${this.props.name}.jpg.to`} />
-    </div>
+    </div>)
   }
-})
+}
 
 {{PascalName}}.propTypes = {
   name: PropTypes.string.isRequired

30efb3f8ad16d41591c722f428ccf8e9c109473a
Joey Baker <joey@byjoeybaker.com>
2015-05-04 09:46:52 -0700

    Add pureRender to react template

--- react-component-template/index.jsx
+++ react-component-template/index.jsx
@@ -1,7 +1,15 @@
 import React, {PropTypes, Component} from 'react'
+import {addons} from 'react/addons'
+const {shouldComponentUpdate} = addons.PureRenderMixin
 const namespace = '{{camelName}}'
 
 export default class {{PascalName}} extends Component {
+  // use the pure-render mixin without the mixin. This allows us to use es6
+  // classes and avoid "magic" code
+  shouldComponentUpdate (...args) {
+    return shouldComponentUpdate.apply(this, args)
+  }
+
   render () {
     return (<div className={namespace}>
       <h1 className={`${namespace}-title`}>{this.props.name} component</h1>

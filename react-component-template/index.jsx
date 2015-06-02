185fa0d965dfd1deb2815db8eb42f9372a684389
Joey Baker <joey@byjoeybaker.com>
2015-06-02 14:51:25 -0700

    add: note about react-router use

--- react-component-template/index.jsx
+++ react-component-template/index.jsx
@@ -5,7 +5,9 @@ const namespace = '{{camelName}}'
 
 export default class {{PascalName}} extends Component {
   // use the pure-render mixin without the mixin. This allows us to use es6
-  // classes and avoid "magic" code
+  // classes and avoid "magic" code. NOTE: if this component is used directly
+  // by react-router, you should delete it, otherwise, the <Link> component will
+  // not cause a re-render
   shouldComponentUpdate (...args) {
     return shouldComponentUpdate.apply(this, args)
   }

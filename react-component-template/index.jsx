f0efb7f1c392f16b9bbfc3c50ed79a6b3be1e23d
Joey Baker <joey@byjoeybaker.com>
2015-07-06 10:48:25 -0700

    fix: a11y correction on react template

--- react-component-template/index.jsx
+++ react-component-template/index.jsx
@@ -15,7 +15,7 @@ export default class {{PascalName}} extends Component {
   render () {
     return (<div>
       <h1 className={styles.title} ref="title">{this.props.name} component</h1>
-      <img src={`http://loremflickr.com/600/600/${this.props.name}`} />
+      <img src={`http://loremflickr.com/600/600/${this.props.name}`} alt={this.props.name} />
     </div>)
   }
 }

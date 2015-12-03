2bfceeb3955f2ddae4359deb63f21fce5b6eb7be
Christopher Stumph <cjstumph@gmail.com>
2015-12-03 14:37:37 -0800

    Don't overwrite files by mistake!
    This is helpful we've often had people accidentally overwrite their work when accidentally using terminal cmd history intending to "run dev" not "run gen"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var hbsDir = require('hbs-dir')
+  , fs = require('fs')
   , change = require('change-case')
   , path = require('path')
   , typeMap = {
@@ -16,21 +17,24 @@ module.exports = function ribcageGen (options, cb) {
   if (!options) throw new Error('options are required')
   if (!typeMap[options.type]) throw new Error('type ' + options.type + ' not found')
 
+  var target = path.join(process.cwd(), options.target)
+  var exists = fs.existsSync(target)
+
   var nameParts = options.target.split(path.sep)
   , name = nameParts[nameParts.length - 1]
   , opts = {
     origin: path.join(__dirname, typeMap[options.type])
-    , target: path.join(process.cwd(), options.target)
+    , target: target
     , context: {
         PascalName: change.pascalCase(name)
         , camelName: change.camelCase(name)
         , titleName: change.titleCase(name)
         , name: name
+        , exists: exists
       }
   }
   , successCallback = function successCallback () {
     cb(opts.context)
   }
-
   hbsDir(opts, successCallback)
 }
